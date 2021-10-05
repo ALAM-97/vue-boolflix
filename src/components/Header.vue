8c540a2b1ab5e0221d8c0c5b1bd217daf9b27998
ALAM-97 <alexamara97@gmail.com>
2021-10-05 13:45:11 +0200

    Components Added

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <form>
+            <input v-model="userInput" type="text" >
+            <button @click.prevent="$emit('search', userInput)">Cerca</button>
+        </form>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    data() {
+        return {
+            userInput: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+    <main>
+        <Films/>
+    </main>
+</template>
+
+<script>
+import axios from 'axios';
+import Films from './Films.vue';
+
+export default {
+    name: 'Main',
+    components: {
+        Films
+    },
+    props: ['info'],
+    data() {
+        return {
+            foundFilms: [],
+        }
+    },
+    created() {
+        axios
+            .get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: 'cdcfe1113982652506af0e8193d0dd64',
+                    query: 'ritorno dal futuro', //da importare dalla search,
+                    language: 'it-IT'
+                }
+            })
+    }
+}
+
+</script>
+
+<style>
+
+</style>
